20139dff786724fda70817f22e25e499fe017bfe
eddie <eddiem9657@gmail.com>
2019-11-02 14:54:02 -0400

    Added Interface + Dice && Die

---
+++ src/main/java/io/zipcoder/casino/Dice.java
@@ -0,0 +1,39 @@
+package io.zipcoder.casino;
+
+public class Dice {
+    private final Integer numberOfFaces;
+    private final Die[] dice;
+
+    public Dice(Integer numberOfDice, Integer numberOfFaces) {
+        this.numberOfFaces = numberOfFaces;
+        this.dice = new Die[numberOfDice];
+        for (int currentIndex = 0; currentIndex < dice.length; currentIndex++) {
+            dice[currentIndex] = new Die(numberOfFaces);
+        }
+    }
+
+    public Integer rollAndSum() {
+        Integer total = 0;
+        for (int currentIndex = 0; currentIndex < dice.length; currentIndex++) {
+            Die die = dice[currentIndex];
+            die.roll();
+            Integer valueRolled = die.getCurrentFacesValue();
+            total += valueRolled;
+        }
+        return total;
+    }
+
+    public Integer getMinRoll() {
+        Integer minimumValueToBeRolled = getNumberOfDice();
+        return minimumValueToBeRolled;
+    }
+
+
+    public Integer getMaxRoll() {
+        return getNumberOfDice() * numberOfFaces;
+    }
+
+    public Integer getNumberOfDice() {
+        return dice.length;
+    }
+}
