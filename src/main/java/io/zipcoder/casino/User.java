4459732636fc3276ab800cd6f1f2358a3b719019
wes <wes.jones1597@gmail.com>
2019-11-02 14:24:34 -0400

    add boilerplate for Card, DeckOfCards, User, UserVillage, UserDisplay

---
+++ src/main/java/io/zipcoder/casino/User.java
@@ -0,0 +1,19 @@
+package io.zipcoder.casino;
+
+public class User {
+    private String name;
+    private Integer userId;
+    private Integer age;
+    private Double wallet;
+
+    public User(String name, Integer userId, Integer age, Double wallet) {
+        this.name = name;
+        this.userId = userId;
+        this.age = age;
+        this.wallet = wallet;
+    }
+
+    public void chooseGame() {
+
+    }
+}
---
+++ src/main/java/io/zipcoder/casino/UserVillage.java
@@ -0,0 +1,20 @@
+package io.zipcoder.casino;
+
+import java.util.ArrayList;
+
+public class UserVillage {
+    private ArrayList<User> users;
+    private User dealer;//maybe?
+
+    public UserVillage() {
+        this.users = new ArrayList<User>();
+    }
+
+    public void createNewUser() {
+
+    }
+
+    public User getUserByUsername(String username) {
+        return null;
+    }
+}
