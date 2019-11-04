2a6e6cb814ef0f35c386d5fc083d162592eb4639
Kievina <56592501+Kievina@users.noreply.github.com>
2019-11-04 17:52:58 -0500

    before merge with dev

--- src/main/java/io/zipcoder/casino/klondike/KlondikeGameEngine.java
+++ src/main/java/io/zipcoder/casino/klondike/KlondikeGameEngine.java
@@ -1,13 +1,16 @@
 package io.zipcoder.casino.klondike;
 
 public class KlondikeGameEngine {
+
     private User user;
     private Dealer dealer;
     private KlondikePlayer klondikePlayer;
     private KlondikeDealer klondikePlayer;
     private KlondikeGame klondikeGame;
 
-    public void displayKlondikeMenu(){}
+    public void displayKlondikeMenu(){
+
+    }
     public Integer chooseAction() {return null;}
     public Boolean getWinner() {return null;}
 
