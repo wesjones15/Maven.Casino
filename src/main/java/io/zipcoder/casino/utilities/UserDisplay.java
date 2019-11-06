86eead4e6e8fe359d1b511ce1e45281904b9aaa7
michael-doll <MWDoll88@gmail.com>
2019-11-06 11:21:52 -0500

    removed language from chooseGame, added age/wallet validation, started UserDisplay Testing

--- src/main/java/io/zipcoder/casino/utilities/UserDisplay.java
+++ src/main/java/io/zipcoder/casino/utilities/UserDisplay.java
@@ -13,33 +13,33 @@ public class UserDisplay {
     private User user;
     private String name;
 
-
-    public Boolean verifyAge() {
-        return null;
-    }
-
-    private void displayMenu() {
-        // 1 : login
-        // 2 : exit
-        // Choose action:
-//        promptForUserCreation(Console.getIntegerInput("Welcome to casino\n" +
-//                "1 - login\n" +
-//                "2 - Exit");
-    }
-    private void promptForUserCreation(int i) {
-        switch(i){
-
-        }
-    }
-    private String promptForName(String name) {
-        return null;
-    }
-    private String promptForExistingUser(Integer userId, String name) {
-        return "Welcome to Casino";
-    }
-    private void promptForGameChoice() {
-
-    }
+ // Not currently Using
+//    public Boolean verifyAge() {
+//        return null;
+//    }
+//
+//    private void displayMenu() {
+//        // 1 : login
+//        // 2 : exit
+//        // Choose action:
+////        promptForUserCreation(Console.getIntegerInput("Welcome to casino\n" +
+////                "1 - login\n" +
+////                "2 - Exit");
+//    }
+//    private void promptForUserCreation(int i) {
+//        switch(i){
+//
+//        }
+//    }
+//    private String promptForName(String name) {
+//        return null;
+//    }
+//    private String promptForExistingUser(Integer userId, String name) {
+//        return "Welcome to Casino";
+//    }
+//    private void promptForGameChoice() {
+//
+//    }
 
 //    BlackJackGameEngine
 //    GoFishGameEngine
@@ -54,7 +54,7 @@ public class UserDisplay {
     }
 
     public static void run() {
-//        UserVillage userVillage = new UserVillage();
+//        UserVillage userVillage = new UserVillage(); Not currently using userVillage
         User user = createCasinoUser();
         int action = displayOptions("create new user", "login", "exit");
         executeAction(action);
@@ -64,52 +64,87 @@ public class UserDisplay {
 
     public static User createCasinoUser() {
         String name = Console.getStringInput("name: ");
-        Integer age = Console.getIntegerInput("age: ");
-        Double wallet = Console.getDoubleInput("wallet: ");
+        Integer age = validateAge();
+        Double wallet = validateWallet();
         return  new User(name, 2, age, wallet);
     }
 
-    public  static void executeAction(int action) {
+    public static Double validateWallet(){
+        Double wallet = Console.getDoubleInput("wallet: ");
+            if(wallet < 0){
+                Console.println("Wallet must be greater than $0.00"); //
+                validateWallet();
+            }
+            return wallet;
+    }
+
+    public static Integer validateAge(){
+        Integer age = Console.getIntegerInput("age: ");
+        if(age < 18){
+            Console.println("Must be at least 18 to enter..");
+            UserDisplay.createCasinoUser();
+        }
+        return age;
+    }
+
+
+
+    public  static String executeAction(int action) {
+        String stringForTesting = "";
+
         switch(action) {
             case 1:
                 // create new user
 //                userVillage.createNewUser();
+                stringForTesting = "create new user";
                 Console.println("creating user");
                 break;
             case 2:
                 // login with existing user
+                stringForTesting = "login with existing user";
                 break;
             case 3:
                 //exit casino
+                stringForTesting = "exit casino";
                 System.exit(0);
                 break;
             default:
+                stringForTesting = "default";
                 break;
         }
+        return stringForTesting;
     }
-    public  static void chooseGame(int action, User user) {
+    public  static String chooseGame(int action, User user) {
+        String stringForTesting = " ";
         switch(action) {
             case 1:
                 // gofish
+                stringForTesting = "gofish";
                 GoFishGameEngine gfge = new GoFishGameEngine(user);
 //                gfge.runGame();
                 break;
             case 2:
                 // black jack
+                stringForTesting = "black jack";
                 break;
             case 3:
                 // klondike
+                stringForTesting = "klondike";
                 break;
             case 4:
                 // craps
+                stringForTesting = "craps";
                 CrapsGameEngine cge = new CrapsGameEngine(user);
 //                cge.displayCrapsMenu();
             case 5:
+                stringForTesting = "Leaving";
                 Console.println("Thank you!, Come again");
                 Console.exit();
             default:
-                Console.println("Bitch wrong input, try again!");
+                stringForTesting = "default";
                 break;
         }
+        return stringForTesting;
     }
+
 }
