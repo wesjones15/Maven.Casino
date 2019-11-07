02dd234a596b929168b173696e026f3facd9feeb
michael-doll <MWDoll88@gmail.com>
2019-11-07 14:43:26 -0500

    remove print methods from Casino, remove unusedUserDisplay Methods

--- src/main/java/io/zipcoder/casino/utilities/UserDisplay.java
+++ src/main/java/io/zipcoder/casino/utilities/UserDisplay.java
@@ -1,66 +1,21 @@
 package io.zipcoder.casino.utilities;
 
-import io.zipcoder.casino.games.CardGames.blackjack.BlackJackGame;
 import io.zipcoder.casino.games.CardGames.blackjack.BlackJackGameEngine;
 import io.zipcoder.casino.games.CardGames.gofish.GoFishGameEngine;
 import io.zipcoder.casino.games.DiceGames.craps.CrapsGameEngine;
 import io.zipcoder.casino.games.DiceGames.klondike.KlondikeGameEngine;
 import io.zipcoder.casino.userandplayer.User;
-import io.zipcoder.casino.userandplayer.UserVillage;
 
 
-import io.zipcoder.casino.utilities.Console;
+
 
 public class UserDisplay {
-    private UserVillage userVillage;
-    private User user;
-    private String name;
-
- // Not currently Using
-//    public Boolean verifyAge() {
-//        return null;
-//    }
-//
-//    private void displayMenu() {
-//        // 1 : login
-//        // 2 : exit
-//        // Choose action:
-////        promptForUserCreation(Console.getIntegerInput("Welcome to casino\n" +
-////                "1 - login\n" +
-////                "2 - Exit");
-//    }
-//    private void promptForUserCreation(int i) {
-//        switch(i){
-//
-//        }
-//    }
-//    private String promptForName(String name) {
-//        return null;
-//    }
-//    private String promptForExistingUser(Integer userId, String name) {
-//        return "Welcome to Casino";
-//    }
-//    private void promptForGameChoice() {
-//
-//    }
-
-//    BlackJackGameEngine
-//    GoFishGameEngine
-//    CrapsGameEngine
-//    KlondikeGameEngine
 
     public static Integer displayOptions(String... options) {
         for (int i = 0; i < options.length; i++) {
             Console.println("%s : %s", i+1, options[i]);
         }
         return Console.getIntegerInput("Select your action: ");
-    }
-
-    public static void run() {
-//        UserVillage userVillage = new UserVillage(); Not currently using userVillage
-        User user = createCasinoUser();
-        int action = displayOptions("create new user", "login", "exit");
-        executeAction(action);
     }
 
     public static User createCasinoUser() {
@@ -77,32 +32,6 @@ public class UserDisplay {
         return  new User(name, 2, age, wallet);
     }
 
-
-    public  static String executeAction(int action) {
-        String stringForTesting = "";
-
-        switch(action) {
-            case 1:
-                // create new user
-//                userVillage.createNewUser();
-                stringForTesting = "create new user";
-                Console.println("creating user");
-                break;
-            case 2:
-                // login with existing user
-                stringForTesting = "login with existing user";
-                break;
-            case 3:
-                //exit casino
-                stringForTesting = "exit casino";
-                System.exit(0);
-                break;
-            default:
-                stringForTesting = "default";
-                break;
-        }
-        return stringForTesting;
-    }
     public  static String chooseGame(int action, User user) {
         String stringForTesting = " ";
         switch(action) {
--- src/test/java/io/zipcoder/casino/utilities/UserDisplayTest.java
+++ src/test/java/io/zipcoder/casino/utilities/UserDisplayTest.java
@@ -48,10 +48,7 @@ public class UserDisplayTest {
         Double actual = user.getWallet();
         Assert.assertEquals(expected,actual);
     }
-    @Test
-    public void testValidateWallet(){
-        //Unable to test currently
-    }
+
 
     @Test
     public void testGetAge() {
@@ -64,102 +61,7 @@ public class UserDisplayTest {
         Integer actual = user.getAge();
         Assert.assertEquals(expected,actual);
     }
-    @Test
-    public void validateAge(){
-        //Unable to test currently
-    }
 
-    @Test
-    public void executeAction1() {
-        String expected = "create new user";
-        String actual = UserDisplay.executeAction(1);
 
-        Assert.assertEquals(expected,actual);
-    }
 
-    @Test
-    public void executeAction2() {
-        String expected = "login with existing user";
-        String actual = UserDisplay.executeAction(2);
-
-        Assert.assertEquals(expected,actual);
-    }
-
-    @Test
-    public void executeAction3() {
-        String expected = "exit casino";
-        String actual = UserDisplay.executeAction(3);
-
-        Assert.assertEquals(expected,actual);
-    }
-
-    @Test
-    public void executeAction4() {
-        String expected = "default";
-        String actual = UserDisplay.executeAction(3);
-
-        Assert.assertEquals(expected,actual);
-    }
-
-// These tests call the game engines , which prompt for input. Re evaluate
-    @Test
-    public void chooseGame1() {
-
-        User user = new User("name",2, 19, 1.0);
-
-        String expected = "gofish";
-        String actual = UserDisplay.chooseGame(1,user);
-
-        Assert.assertEquals(expected,actual);
-    }
-    @Test
-    public void chooseGame2() {
-
-        User user = new User("name",2, 19, 1.0);
-
-        String expected = "black jack";
-        String actual = UserDisplay.chooseGame(2,user);
-
-        Assert.assertEquals(expected,actual);
-    }
-    @Test
-    public void chooseGame3() {
-
-        User user = new User("name",2, 19, 1.0);
-
-        String expected = "klondike";
-        String actual = UserDisplay.chooseGame(3,user);
-
-        Assert.assertEquals(expected,actual);
-    }
-    @Test
-    public void chooseGame4() {
-
-        User user = new User("name",2, 19, 1.0);
-
-        String expected = "craps";
-        String actual = UserDisplay.chooseGame(4,user);
-
-        Assert.assertEquals(expected,actual);
-    }
-    @Test
-    public void chooseGame5() {
-
-        User user = new User("name",2, 19, 1.0);
-
-        String expected = "Leaving";
-        String actual = UserDisplay.chooseGame(5,user);
-
-        Assert.assertEquals(expected,actual);
-    }
-    @Test
-    public void chooseGame6() {
-
-        User user = new User("name",2, 19, 1.0);
-
-        String expected = "default";
-        String actual = UserDisplay.chooseGame(6,user);
-
-        Assert.assertEquals(expected,actual);
-    }
 }
